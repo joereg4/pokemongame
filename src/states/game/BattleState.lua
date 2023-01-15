7aa05e00617e6219f584dd7371f3e094f5b5ffbc
joereg4 <joereg4@gmail.com>
2023-01-14 19:41:29 -0600

    Increased Opponent Level

--- src/states/game/BattleState.lua
+++ src/states/game/BattleState.lua
@@ -5,8 +5,7 @@
     Author: Colton Ogden
     cogden@cs50.harvard.edu
 ]]
-
-BattleState = Class{__includes = BaseState}
+BattleState = Class {__includes = BaseState}
 
 function BattleState:init(player)
     self.player = player
@@ -15,47 +14,49 @@ function BattleState:init(player)
     -- flag for when the battle can take input, set in the first update call
     self.battleStarted = false
 
-    self.opponent = Opponent {
+    self.opponent =
+        Opponent {
         party = Party {
             pokemon = {
-                Pokemon(Pokemon.getRandomDef(), math.random(2, 6))
+                Pokemon(Pokemon.getRandomDef(), math.random(2, 20))
             }
         }
     }
 
-    self.playerSprite = BattleSprite(self.player.party.pokemon[1].battleSpriteBack, 
-        -64, VIRTUAL_HEIGHT - 128)
-    self.opponentSprite = BattleSprite(self.opponent.party.pokemon[1].battleSpriteFront, 
-        VIRTUAL_WIDTH, 8)
+    self.playerSprite = BattleSprite(self.player.party.pokemon[1].battleSpriteBack, -64, VIRTUAL_HEIGHT - 128)
+    self.opponentSprite = BattleSprite(self.opponent.party.pokemon[1].battleSpriteFront, VIRTUAL_WIDTH, 8)
 
     -- health bars for pokemon
-    self.playerHealthBar = ProgressBar {
+    self.playerHealthBar =
+        ProgressBar {
         x = VIRTUAL_WIDTH - 160,
         y = VIRTUAL_HEIGHT - 80,
         width = 152,
         height = 6,
-        color = {r = 189/255, g = 32/255, b = 32/255},
+        color = {r = 189 / 255, g = 32 / 255, b = 32 / 255},
         value = self.player.party.pokemon[1].currentHP,
         max = self.player.party.pokemon[1].HP
     }
 
-    self.opponentHealthBar = ProgressBar {
+    self.opponentHealthBar =
+        ProgressBar {
         x = 8,
         y = 8,
         width = 152,
         height = 6,
-        color = {r = 189/255, g = 32/255, b = 32/255},
+        color = {r = 189 / 255, g = 32 / 255, b = 32 / 255},
         value = self.opponent.party.pokemon[1].currentHP,
         max = self.opponent.party.pokemon[1].HP
     }
 
     -- exp bar for player
-    self.playerExpBar = ProgressBar {
+    self.playerExpBar =
+        ProgressBar {
         x = VIRTUAL_WIDTH - 160,
         y = VIRTUAL_HEIGHT - 73,
         width = 152,
         height = 6,
-        color = {r = 32/255, g = 32/255, b = 189/255},
+        color = {r = 32 / 255, g = 32 / 255, b = 189 / 255},
         value = self.player.party.pokemon[1].currentExp,
         max = self.player.party.pokemon[1].expToLevel
     }
@@ -73,11 +74,10 @@ function BattleState:init(player)
 end
 
 function BattleState:enter(params)
-    
 end
 
 function BattleState:exit()
-    gSounds['battle-music']:stop()
+    gSounds["battle-music"]:stop()
     -- gSounds['field-music']:play()
 end
 
@@ -89,11 +89,11 @@ function BattleState:update(dt)
 end
 
 function BattleState:render()
-    love.graphics.clear(214/255, 214/255, 214/255, 1)
+    love.graphics.clear(214 / 255, 214 / 255, 214 / 255, 1)
 
-    love.graphics.setColor(45/255, 184/255, 45/255, 124/255)
-    love.graphics.ellipse('fill', self.opponentCircleX, 60, 72, 24)
-    love.graphics.ellipse('fill', self.playerCircleX, VIRTUAL_HEIGHT - 64, 72, 24)
+    love.graphics.setColor(45 / 255, 184 / 255, 45 / 255, 124 / 255)
+    love.graphics.ellipse("fill", self.opponentCircleX, 60, 72, 24)
+    love.graphics.ellipse("fill", self.playerCircleX, VIRTUAL_HEIGHT - 64, 72, 24)
 
     love.graphics.setColor(1, 1, 1, 1)
     self.opponentSprite:render()
@@ -106,12 +106,18 @@ function BattleState:render()
 
         -- render level text
         love.graphics.setColor(0, 0, 0, 1)
-        love.graphics.setFont(gFonts['small'])
-        love.graphics.print('LV ' .. tostring(self.playerPokemon.level),
-            self.playerHealthBar.x, self.playerHealthBar.y - 10)
-        love.graphics.print('LV ' .. tostring(self.opponentPokemon.level),
-            self.opponentHealthBar.x, self.opponentHealthBar.y + 8)
-        love.graphics.setFont(gFonts['medium'])
+        love.graphics.setFont(gFonts["small"])
+        love.graphics.print(
+            "LV " .. tostring(self.playerPokemon.level),
+            self.playerHealthBar.x,
+            self.playerHealthBar.y - 10
+        )
+        love.graphics.print(
+            "LV " .. tostring(self.opponentPokemon.level),
+            self.opponentHealthBar.x,
+            self.opponentHealthBar.y + 8
+        )
+        love.graphics.setFont(gFonts["medium"])
         love.graphics.setColor(1, 1, 1, 1)
     end
 
@@ -122,30 +128,38 @@ function BattleState:triggerSlideIn()
     self.battleStarted = true
 
     -- slide the sprites and circles in from the edges, then trigger first dialogue boxes
-    Timer.tween(1, {
-        [self.playerSprite] = {x = 32},
-        [self.opponentSprite] = {x = VIRTUAL_WIDTH - 96},
-        [self] = {playerCircleX = 66, opponentCircleX = VIRTUAL_WIDTH - 70}
-    })
-    :finish(function()
-        self:triggerStartingDialogue()
-        self.renderHealthBars = true
-    end)
+    Timer.tween(
+        1,
+        {
+            [self.playerSprite] = {x = 32},
+            [self.opponentSprite] = {x = VIRTUAL_WIDTH - 96},
+            [self] = {playerCircleX = 66, opponentCircleX = VIRTUAL_WIDTH - 70}
+        }
+    ):finish(
+        function()
+            self:triggerStartingDialogue()
+            self.renderHealthBars = true
+        end
+    )
 end
 
 function BattleState:triggerStartingDialogue()
-    
     -- display a dialogue first for the pokemon that appeared, then the one being sent out
-    gStateStack:push(BattleMessageState('A wild ' .. tostring(self.opponent.party.pokemon[1].name ..
-        ' appeared!'),
-    
-    -- callback for when the battle message is closed
-    function()
-        gStateStack:push(BattleMessageState('Go, ' .. tostring(self.player.party.pokemon[1].name .. '!'),
-    
-        -- push a battle menu onto the stack that has access to the battle state
-        function()
-            gStateStack:push(BattleMenuState(self))
-        end))
-    end))
+    gStateStack:push(
+        BattleMessageState(
+            "A wild " .. tostring(self.opponent.party.pokemon[1].name .. " appeared!"),
+            -- callback for when the battle message is closed
+            function()
+                gStateStack:push(
+                    BattleMessageState(
+                        "Go, " .. tostring(self.player.party.pokemon[1].name .. "!"),
+                        -- push a battle menu onto the stack that has access to the battle state
+                        function()
+                            gStateStack:push(BattleMenuState(self))
+                        end
+                    )
+                )
+            end
+        )
+    )
 end
